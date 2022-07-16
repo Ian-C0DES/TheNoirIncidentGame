3f1eafec5e3acff8b61da9715abb6d34ba76bf52
strem <tsheats2@ggc.edu>
2022-07-16 15:39:32 -0400

    (2)added rooms: descriptions + names

--- GameDB.sql
+++ GameDB.sql
@@ -37,6 +37,19 @@ INSERT INTO [Rooms] ([RoomID], [RoomName], [RoomDescription], [InstanceName],[Pl
 --'Abandonded Club', 'Red Light', 'Red lights are flickering. You remember Alex''s favorite color is red. Nothing is really in this room to see so you exit.'
 --'Abandonded Club', 'Edge', 'This is the only room that has not been checked. You began to walk back out then notice something on the floor. It is Alex''s ring that his father gave him.'
 
+--'Ex-Courthouse/Gambling site', 'King', 'You walk up the steps to this ex courthouse. Seems very ran down. Tons of broken bricks inside and sheets pinned over some windows.'
+--'Ex-Courthouse/Gambling site', 'Ace of Spades', 'In the midst of walking, it’s only ace cards around. Seems like thsis may be a gambling spot.'
+--'Ex-Courthouse/Gambling site', 'Lucky', 'The room has bright glare coming from the broken window inside. Coming through the window is a vein of four leaf clovers and other plants. With insects over the floor.'
+--'Ex-Courthouse/Gambling site', 'Poker Face', 'Gambling spot has no clue of people so far. Many dice are on the floor. You see ripped games cards everywhere. '
+--'Ex-Courthouse/Gambling site', 'Turned Tables', 'There are some voices coming from this room. Walks in a see a group of men shooting dice. They have no idea of Alex. Just beer bottles and money around.'
+--'Ex-Courthouse/Gambling site', 'Jackpot', 'The sixth room is being approached. Floor is filthy with trash and smells like dried up blood. No one has even touched this room, just spider webs and dust collecting.'
+
+--'Diner', 'Floor', 'As you approach this diner, something smells rotten. Looks over to see what it is, just some rotten food throwed out. First thing you see is a waiter and a few customers inside eating. Nothing looks sanitary here.'
+--'Diner', 'Kitchen', 'Proceeds to walk into kitchen. So far all that you can see is a few out dated food products and silverware.'
+--'Diner', 'Freezer', 'Inside of the kitchen is a freezer. You open it up, and it is pretty much empty. A few frozen rats in the corner.'
+--'Diner', 'Cleaning Room', 'You open up this dirty black door. Inside is a mop, a few cleaning sprays and dirty rags.'
+--'Diner', 'Dumpster', 'Head to back room to see what is here. Just a huge dumpster with trash pilled to the top. Mostly broken down boxes and some old food.'
+
 --melissa
 
 
